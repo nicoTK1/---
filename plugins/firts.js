7e4fd30d1ab4fb3b09a960c5f49ca59bbebb94b6
nicoTK1 <147124577+nicoTK1@users.noreply.github.com>
2024-03-23 03:19:33 -0300

    Update firts.js

--- plugins/firts.js
+++ plugins/firts.js
@@ -7,14 +7,14 @@ if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup) return
 let user = global.db.data.users[m.sender]
 
 if (new Date() - user.pc < 21600000) return
-await m.reply(`👋 Hola ${username}¡!
+await m.reply(`👋 𝘏𝘖𝘓𝘈 ${username}¡!
  *${saludo}*
 
 *⚠️ Nota:* No Envié Spam Al Bot!
 ⚡️ Escriba !menu Para Ver Los Comandos Que Dispongo!
 
 *🥀 Si Tienes Duda O Sugerencia Contacta A Mi Creador: 📍*
-💖 +57 301 2482597 🧸
+💖 +543513610400 🧸
 
 *⚡️ Escribe !grupos Para Ver Los Grupos Oficiales Del Bot 🦊*
 
