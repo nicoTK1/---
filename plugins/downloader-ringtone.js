ce8b90ae1ea13c106cc15f32586eb85885ec6b3e
nicoo <147124577+nicoTK1@users.noreply.github.com>
2024-03-23 20:44:03 -0300

    Update downloader-ringtone.js

--- plugins/downloader-ringtone.js
+++ plugins/downloader-ringtone.js
@@ -1,3 +1,4 @@
+//comando de https://github.com/nicoTK1/NicoBotLite-MD
 import fetch from 'node-fetch';
 const handler = async (m, {conn, groupMetadata, usedPrefix, text, args, command}) => {
   if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} Hola*`;
