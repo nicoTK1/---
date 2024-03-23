f81eedfc9559a8102474fa4cc731b08af8d655b4
nicoTK1 <147124577+nicoTK1@users.noreply.github.com>
2024-03-23 07:05:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('SakuraBot\nLite-MD', {
+say('NicoBot\nLite-MD', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
