cbbefc67a6206588bb7dbfa8232a3272af5fa78a
nicoTK1 <147124577+nicoTK1@users.noreply.github.com>
2024-03-23 17:06:39 -0300

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,6 +1,6 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = false;
-  m.reply('✅️ *Chat Desbaneado Con Exito, Ya Pueden Usar SakuraBotLite - MD*');
+  m.reply('✅️ 𝘊𝘏𝘈𝘛 𝘋𝘌𝘚𝘉𝘈𝘕𝘌𝘈𝘋𝘖 𝘊𝘖𝘕 𝘌𝘟𝘐𝘛𝘖, 𝘠𝘈 𝘗𝘜𝘌𝘋𝘌𝘕 𝘜𝘚𝘈𝘙 𝙉𝙞𝙘𝙤𝘽𝙤𝙩𝙇𝙞𝙩𝙚 - 𝙈𝘿');
 };
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
