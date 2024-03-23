efc216ec5c228e0fbd36e5b11ee858f162e8ee5a
nicoTK1 <147124577+nicoTK1@users.noreply.github.com>
2024-03-23 02:14:01 -0300

    Update _antilink2.js

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -20,8 +20,8 @@ export async function before(m, {conn, isAdmin, isBotAdmin, text}) {
       if (m.text.includes(linkThisGroup2)) return !0;
       if (m.text.includes(linkThisGroup3)) return !0;
     }
-    await this.sendMessage(m.chat, {text: `*¡Enlace detectado!, Mandaste un enlace prohibido por lo cual seras eliminado*`, mentions: [m.sender]}, {quoted: m});
-    if (!isBotAdmin) return m.reply('🌹 *No soy admin, no puedo eliminar intrusos*');
+    await this.sendMessage(m.chat, {text: `*❌ 𝙀𝙉𝙇𝘼𝘾𝙀 𝙋𝙍𝙊𝙃𝙄𝘽𝙄𝘿𝙊 ( 𝘽𝘼𝙉𝙀𝘼𝘿𝙊 ) ⚡*`, mentions: [m.sender]}, {quoted: m});
+    if (!isBotAdmin) return m.reply('🌹 *𝙉𝙊 𝙎𝙊𝙔 𝘼𝘿𝙈𝙄𝙉, 𝙉𝙊 𝙋𝙐𝙀𝘿𝙊 𝙀𝙇𝙄𝙈𝙄𝙉𝘼𝙍 𝘼 𝙀𝙎𝙀 𝙂𝘼𝙔 💀*');
     if (isBotAdmin && bot.restrict) {
       await conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
       const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
