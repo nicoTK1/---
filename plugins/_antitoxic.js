95450f90fac6d8bfbd6e318c777872effdc8d791
nicoTK1 <147124577+nicoTK1@users.noreply.github.com>
2024-03-23 02:48:53 -0300

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,4 +1,4 @@
-const toxicRegex = /puto|puta|rata|estupido|imbecil|rctmre|mrd|verga|vrga|maricon/i;
+const toxicRegex = /puto|puta|down|mogolico|idiota|feo|fea|rata|estupido|imbecil|rctmre|mrd|verga|vrga|maricon/i;
 
 export async function before(m, {isAdmin, isBotAdmin, isOwner}) {
   if (m.isBaileys && m.fromMe) {
@@ -14,12 +14,12 @@ export async function before(m, {isAdmin, isBotAdmin, isOwner}) {
 
   if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
     user.warn += 1;
-    if (!(user.warn >= 5)) await m.reply('*🍧 ' + `${user.warn == 1 ? `Hola @${m.sender.split`@`[0]}` : `@${m.sender.split`@`[0]}`}, decir la palabra "${isToxic}" está prohibido en este grupo. Advertencia: ${user.warn}/5.` + '*', false, {mentions: [m.sender]});
+    if (!(user.warn >= 5)) await m.reply('*🍧 ' + `${user.warn == 1 ? `𝘏𝘖𝘓𝘈 @${m.sender.split`@`[0]}` : `@${m.sender.split`@`[0]}`}, 𝘋𝘌𝘊𝘐𝘙 𝘓𝘈 𝘗𝘈𝘓𝘈𝘉𝘙𝘈 "${isToxic}" 𝘌𝘚𝘛𝘈 𝙋𝙍𝙊𝙃𝙄𝘽𝙄𝘿𝙊 𝘌𝘕 𝘌𝘚𝘛𝘌 𝘎𝘙𝘜𝘗𝘖. 𝐀𝐃𝐕𝐄𝐑𝐓𝐄𝐍𝐂𝐈𝐀 : ${user.warn}/5.` + '*', false, {mentions: [m.sender]});
   }
 
   if (user.warn >= 5) {
     user.warn = 0;
-    await m.reply(`*🥀 Hola @${m.sender.split`@`[0]}, superaste las 5 advertencias por lo que serás eliminado de este grupo por tu comportamiento.*`, false, {mentions: [m.sender]});
+    await m.reply(`*🥀 𝘏𝘖𝘓𝘈 @${m.sender.split`@`[0]}, 𝘚𝘜𝘗𝘌𝘙𝘈𝘚𝘛𝘌 𝘓𝘈𝘚 5 𝘈𝘋𝘝𝘌𝘙𝘛𝘌𝘕𝘊𝘐𝘈𝘚 ⚠️ 𝘚𝘌𝘙𝘈𝘚 𝘉𝘈𝘕𝘌𝘈𝘋𝘖/𝘈*`, false, {mentions: [m.sender]});
     user.banned = true;
     await mconn.conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
     // await this.updateBlockStatus(m.sender, 'block')
