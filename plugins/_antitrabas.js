ccb2608d6454c6358a19674af1a0d81a40366d7f
nicoTK1 <147124577+nicoTK1@users.noreply.github.com>
2024-03-23 07:28:41 -0300

    Update _antitrabas.js

--- plugins/_antitrabas.js
+++ plugins/_antitrabas.js
@@ -16,8 +16,8 @@ export async function before(m, {conn, isAdmin, isBotAdmin, usedPrefix}) {
   const name = await conn.getName(m.sender);
   const fakemek = {'key': {'participant': '0@s.whatsapp.net', 'remoteJid': '0@s.whatsapp.net'}, 'message': {'groupInviteMessage': {'groupJid': '51995386439-1616969743@g.us', 'inviteCode': 'm', 'groupName': 'P', 'caption': '𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝', 'jpegThumbnail': null}}};
   if (chat.antiTraba && m.text.length > 5000) { // Cantidad máxima de caracteres aceptados en un mensaje//
-    if (isAdmin) return conn.sendMessage(m.chat, {text: `El administrador @${m.sender.split('@')[0]} acaba de enviar un texto que contiene muchos caracteres -.-!`, mentions: [m.sender]}, {quoted: fakemek});
-    conn.sendMessage(m.chat, `*[ ! ] Se detecto un mensaje que contiene muchos caracteres [ ! ]*\n`, `${isBotAdmin ? '' : 'No soy administrador, no puedo hacer nada :/'}`, m);
+    if (isAdmin) return conn.sendMessage(m.chat, {text: `𝙀𝙡 𝘼𝙙𝙢𝙞𝙣𝙞𝙨𝙩𝙧𝙖𝙙𝙤𝙧 @${m.sender.split('@')[0]} 𝘼𝙘𝙖𝙗𝙖 𝙙𝙚 𝙢𝙖𝙣𝙙𝙖𝙧 𝙪𝙣 𝘽𝙄𝙉𝘼𝙍𝙄𝙊 💀 -.-!`, mentions: [m.sender]}, {quoted: fakemek});
+    conn.sendMessage(m.chat, `*[ ! ] 𝘿𝙀𝙏𝙀𝘾𝙏𝙀 𝙐𝙉 𝙈𝙀𝙉𝙎𝘼𝙅𝙀 𝘾𝙊𝙉 𝙈𝙐𝘾𝙃𝙊𝙎 𝘾𝘼𝙍𝘼𝘾𝙏𝙀𝙍𝙀𝙎 👻 [ ! ]*\n`, `${isBotAdmin ? '' : '𝙉𝙤 𝙨𝙤𝙮 𝘼𝙙𝙢𝙞𝙣𝙞𝙨𝙩𝙧𝙖𝙙𝙤𝙧, 𝙉𝙤 𝙥𝙪𝙚𝙙𝙤 𝙝𝙖𝙘𝙚𝙧 𝙣𝙖𝙙𝙖 💀 :/'}`, m);
     // await conn.sendButton(m.chat, `*[ ! ] Se detecto un mensaje que contiene muchos caracteres [ ! ]*\n`, `${isBotAdmin ? '' : 'No soy administrador, no puedo hacer nada :/'}`, author, ['[ DESACTIVAR ANTI TRABAS ]', usedPrefix+'apagar antitraba'], fakemek )
     if (isBotAdmin && bot.restrict) {
       conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
@@ -27,7 +27,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, usedPrefix}) {
       setTimeout(() => {
         	conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
       }, 1000);
-    } else if (!bot.restrict) return m.reply('[ ! ] Para realizar acciones de eliminación, mi dueño tiene que encender el modo restringido!');
+    } else if (!bot.restrict) return m.reply('[ ! ] 𝘗𝘢𝘳𝘢 𝘳𝘦𝘢𝘭𝘪𝘻𝘢𝘳 𝘢𝘤𝘤𝘪𝘰𝘯𝘦𝘴 𝘥𝘦 𝘦𝘭𝘪𝘮𝘪𝘯𝘢𝘤𝘪ó𝘯, 𝘮𝘪 𝘥𝘶𝘦ñ𝘰 𝘕𝘐𝘊𝘖 𝘵𝘪𝘦𝘯𝘦 𝘲𝘶𝘦 𝘦𝘯𝘤𝘦𝘯𝘥𝘦𝘳 𝘦𝘭 𝘮𝘰𝘥𝘰 𝘳𝘦𝘴𝘵𝘳𝘪𝘯𝘨𝘪𝘥𝘰!');
   }
   return !0;
 }
