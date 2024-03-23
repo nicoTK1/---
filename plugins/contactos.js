e3ab98a434bf76f31c301fa765d4d46194f3ca9e
nicoo <147124577+nicoTK1@users.noreply.github.com>
2024-03-23 20:19:01 -0300

    Update contactos.js

--- plugins/contactos.js
+++ plugins/contactos.js
@@ -1,3 +1,4 @@
+//comando de https://github.com/nicoTK1/NicoBotLite-MD
 function handler(m) {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 const data = global.contactos.filter(([id, isCreator]) => id && isCreator) 
